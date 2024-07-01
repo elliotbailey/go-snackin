af600d34f8277199df9a07ea70d68bb80127b439
Elliot Bailey <elliotbailey999@gmail.com>
2024-07-01 11:22:23 +0800

    Updates with minor api changes and fiddling with react

--- src/api/google-maps.js
+++ src/api/google-maps.js
@@ -29,3 +29,48 @@ export async function autocompleteLocation(input, countries = ['au']) {
     return data.suggestions[0].placePrediction.text.text; // returns name address
     // return data.suggestions[0].placePrediction.placeId; // returns placeID
 }
+
+export async function generateRoutePolyline(d, o = null, travelMode = 'DRIVE') {
+    const headers = {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': process.env.GOOGLE_MAPS_API_KEY,
+        'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
+    };
+
+    // if (origin == null) {
+    //     /// get current location somehow
+    // }
+
+    const body = {
+        destination: {
+            "address": 
+            // "location": {
+            //     "latLng": {
+            //         "latitude": d.latitude,
+            //         "longitude": d.longitude
+            //     }
+            // }
+        },
+        origin: {
+            "location": {
+                "latLng": {
+                    "latitude": o.latitude,
+                    "longitude": o.longitude
+                }
+            }
+        },
+        travelMode: travelMode
+    }
+
+    const response = await fetch(baseURLs.MAPS_ROUTES_URL, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body)
+    });
+
+    const data = await response.json();
+    fs.writeFile('./src/reference/polylineExample.json', JSON.stringify(data), 'utf8', () => {});
+
+    return data;
+    
+}
--- src/server/server.js
+++ src/server/server.js
@@ -4,21 +4,37 @@ dotenv.config();
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
+import polyline from 'google-polyline';
 
 import { processInput } from '../api/natural-language.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export async function startServer() {
 
     const app = express();
+    app.use(cors());
     app.use(express.json());
+    app.use(express.static(path.join(__dirname, '..')));
 
-    app.listen(process.env.PORT, () => {
-        console.log(`Go Snackin' server running on port ${process.env.PORT}`);
+    app.listen(process.env.PORT, '0.0.0.0', () => {
+        console.log(`Go Snackin' Express server running on port ${process.env.PORT}`);
     });
 
     app.post('/api/nlp', async (req, res) => {
         // curl -i -X POST -H 'Content-Type: application/json' -d '{"input":"Lets go from Sydney to Melbourne and grab a beer"}' localhost:3000/api/nlp
         res.status(200).json({ output: await processInput(req.body.input) });
+    });
+
+    app.post('/api/decode', (req, res) => {
+        const decoded = polyline.decode(req.body.input);
+        res.status(200).json({ output: decoded });
+    });
+
+    app.get('/test', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', 'index.html'));
     });
 
 }
