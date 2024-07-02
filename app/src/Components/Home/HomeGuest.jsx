e4d74bd8bdffed47cbc9a1273504c31fa6b7be13
Elliot Bailey <elliotbailey999@gmail.com>
2024-07-03 03:20:36 +0800

    Restructured entire repository, updated gitignore to remove old files and database file

--- app/src/Components/Home/HomeGuest.jsx
+++ app/src/Components/Home/HomeGuest.jsx
@@ -3,11 +3,9 @@ import './Home.css'; // Import CSS file for styling
 
 function HomeGuest() {
 
-
   return (
     <div className="home-container">
       <div className="home-content">
-
       <div className="input-container">
           <div className="curved-input">
             Please log in or sign up to access the program.
