ce898a0fdc49846dca012feaaa7927355a37347b
Elliot Bailey <elliotbailey999@gmail.com>
2024-07-02 21:35:25 +0800

    Updated server to include more comprehensive endpoints, including for first version of complete route generations

--- src/server/server.js
+++ src/server/server.js
@@ -8,6 +8,8 @@ import cors from 'cors';
 import polyline from 'google-polyline';
 
 import { processInput } from '../api/natural-language.js';
+import { generateRoutePolyline, generateRoutePolylineWithWaypoints, searchNearby,
+    convertCoordinatesToCoords } from '../api/google-maps.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,12 +27,45 @@ export async function startServer() {
 
     app.post('/api/nlp', async (req, res) => {
         // curl -i -X POST -H 'Content-Type: application/json' -d '{"input":"Lets go from Sydney to Melbourne and grab a beer"}' localhost:3000/api/nlp
-        res.status(200).json({ output: await processInput(req.body.input) });
+        res.status(200).json({ "output": await processInput(req.body.input) });
     });
 
     app.post('/api/decode', (req, res) => {
         const decoded = polyline.decode(req.body.input);
-        res.status(200).json({ output: decoded });
+        res.status(200).json({ "output": decoded });
+    });
+
+    app.post('/api/polyline', async (req, res) => {
+        const naturalOutput = await processInput(req.body.input);
+        const result = await generateRoutePolyline(
+            naturalOutput.locations.origin,
+            naturalOutput.locations.destination
+        );
+        res.status(200).json({ "polyline": result });
+    });
+
+    app.post('/api/nearby', async (req, res) => {
+        // includes fields for location, type
+        const result = await searchNearby(req.body.location, req.body.included);
+        res.status(200).json({ "output": result });
+    });
+
+    app.post('/api/generateRoute', async (req, res) => {
+        const naturalOutput = await processInput(req.body.input);
+        const origin = naturalOutput.locations.origin;
+        const destination = naturalOutput.locations.destination;
+        const activity = naturalOutput.activity;
+        console.log(`Origin: ${origin}, Destination: ${destination}, Activity: ${activity}`)
+        const routeCoords = polyline.decode(await generateRoutePolyline(origin, destination));
+        const midway = Math.floor(routeCoords.length / 2);
+        const nearby = await searchNearby(convertCoordinatesToCoords(routeCoords[midway]), activity);
+        const newPoly = await generateRoutePolylineWithWaypoints(origin, destination, 'DRIVE', [nearby.location]);
+        res.status(200).json({ 
+            stopLocation: nearby.location,
+            stopName: nearby.displayName.text,
+            stopAddress: nearby.formattedAddress,
+            "polyline": newPoly 
+        });
     });
 
     app.get('/test', (req, res) => {
